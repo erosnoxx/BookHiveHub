1af1894482088001626979238d9524f96fd4c2a7
erosnoxx <garotoslixo@gmail.com>
2024-11-30 01:36:05 -0300

    feat: first commit

---
+++ src/test/java/com/book/hive/hub/HubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.hive.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
